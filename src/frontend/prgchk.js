1f35c92c1c5af9f48c4e481a7c633342a48d88b1
CHISOM96 <chisomnnabuihe@gmail.com>
2020-06-07 21:13:00 +0100

    eslint

--- src/frontend/prgchk.js
+++ src/frontend/prgchk.js
@@ -1,121 +1,115 @@
-//question bank
-var questions = [
-    {
-        question: "Which of the following values is bla bla bla?",
-        answers: {
-            a: 'ipsum',
-            b: 'lorem',
-            c: 'correct answer'
+// question bank
+const questions = [
+  {
+    question: 'Which of the following values is bla bla bla?',
+    answers: {
+      a: 'ipsum',
+      b: 'lorem',
+      c: 'correct answer',
 
-        },
-        correctAnswer: 'c'
     },
-    {
-        question: "Which of the following values is bla bla bla?",
-        answers: {
-            a: 'correct answer',
-            b: 'lorem',
-            c: 'ipsum'
-        },
-        correctAnswer: 'a'
+    correctAnswer: 'c',
+  },
+  {
+    question: 'Which of the following values is bla bla bla?',
+    answers: {
+      a: 'correct answer',
+      b: 'lorem',
+      c: 'ipsum',
     },
-    {
-        question: "Which of the following values is bla bla bla?",
-        answers: {
-            a: 'correct answer',
-            b: 'lorem',
-            c: 'ipsum'
-        },
-        correctAnswer: 'a'
-    }
+    correctAnswer: 'a',
+  },
+  {
+    question: 'Which of the following values is bla bla bla?',
+    answers: {
+      a: 'correct answer',
+      b: 'lorem',
+      c: 'ipsum',
+    },
+    correctAnswer: 'a',
+  },
 ];
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
 
 generateQuiz(questions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+  function showQuestions(questions, quizContainer) {
+    // storage
+    const output = [];
+    let answers;
 
-    function showQuestions(questions, quizContainer) {
-        // storage
-        var output = [];
-        var answers;
+    // question appearance
+    for (let i = 0; i < questions.length; i++) {
+      // first reset the list of answers
+      answers = [];
 
-        // question appearance
-        for (var i = 0; i < questions.length; i++) {
+      // for each available answer...
+      for (letter in questions[i].answers) {
+        // answer buttons
+        answers.push(
+          // '<label>'
+          // + '<input type="button" name="question' + i + '" value="' + letter + '">'
+          // + letter + ': '
+          // + questions[i].answers[letter]
+          // + '</label>'
+          // );
+          `${'<label class="btn btn-secondary answer">'
+                    + '<input type="radio" autocomplete="off" name="question'}${i}" value="${letter}">${
+            letter}: ${
+            questions[i].answers[letter]
+          }</label>`,
+        );
+      }
 
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for (letter in questions[i].answers) {
-
-                // answer buttons
-                answers.push(
-                    // '<label>'
-                    // + '<input type="button" name="question' + i + '" value="' + letter + '">'
-                    // + letter + ': '
-                    // + questions[i].answers[letter]
-                    // + '</label>'
-                    // );
-                    '<label class="btn btn-secondary answer">'
-                    + '<input type="radio" autocomplete="off" name="question' + i + '" value="' + letter + '">'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // question and anwers
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        // combine output
-        quizContainer.innerHTML = output.join('');
+      // question and anwers
+      output.push(
+        `<div class="question">${questions[i].question}</div>`
+                + `<div class="answers">${answers.join('')}</div>`,
+      );
     }
 
+    // combine output
+    quizContainer.innerHTML = output.join('');
+  }
 
-    function showResults(questions, quizContainer, resultsContainer) {
 
-        // answer container
-        var answerContainers = quizContainer.querySelectorAll('.answers');
+  function showResults(questions, quizContainer, resultsContainer) {
+    // answer container
+    const answerContainers = quizContainer.querySelectorAll('.answers');
 
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
+    // keep track of user's answers
+    let userAnswer = '';
+    let numCorrect = 0;
 
-        // for each question...
-        for (var i = 0; i < questions.length; i++) {
+    // for each question...
+    for (let i = 0; i < questions.length; i++) {
+      // find selected answer
+      userAnswer = (answerContainers[i].querySelector(`input[name=question${i}]:checked`) || {}).value;
 
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-            // if answer is correct
-            if (userAnswer === questions[i].correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
-            }
-        }
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-        var total = numCorrect + 0;
-
+      // if answer is correct
+      if (userAnswer === questions[i].correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+      }
     }
 
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+    const total = numCorrect + 0;
+  }
 
-    // show questions right away
-    showQuestions(questions, quizContainer);
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
 
 
-    // on submit, show results
-    submitButton.onclick = function () {
-        showResults(questions, quizContainer, resultsContainer);
-    }
+  // on submit, show results
+  submitButton.onclick = function () {
+    showResults(questions, quizContainer, resultsContainer);
+  };
 }
